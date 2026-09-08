9145edf4529ddae5a1b15e9b054dc7784e9d8a09
Michael Bejarano <michaelbejarano940@gmail.com>
2026-09-07 22:35:37 -0500

    se agrego el certificado.pem

--- convert_cert.php
+++ convert_cert.php
@@ -5,17 +5,17 @@ require 'vendor/autoload.php';
 use Greenter\XMLSecLibs\Certificate\X509Certificate;
 use Greenter\XMLSecLibs\Certificate\X509ContentType;
 
-$pfx = file_get_contents(__DIR__ . '/webroot/certificados/certificadoyuwer.pfx');
+$pfx = file_get_contents(__DIR__ . '/webroot/certificados/certificadojyza.pfx');
 
 // contraseña del certificado
-$password = 'QPdan98user';
+$password = 'Sunat123';
 
 $certificate = new X509Certificate($pfx, $password);
 
 $pem = $certificate->export(X509ContentType::PEM);
 
 file_put_contents(
-    __DIR__ . '/webroot/certificados/certificadoyuwer.pem',
+    __DIR__ . '/webroot/certificados/certificadojyza.pem',
     $pem
 );
 
